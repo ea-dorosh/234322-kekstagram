b47ef2aacf1955d0ab495afe477b7749e27bdf8f
EGOR <ea-dorosh@yandex.ru>
2018-12-02 20:59:46 +0400

    фотографии открываются и закрываются редактор фотографий открывается

--- js/pictures.js
+++ js/pictures.js
@@ -5,6 +5,7 @@ var PHOTO_QUANTITY = 25;
 
 var MAX_LIKES = 200;
 var MIN_LIKES = 15;
+var ESC_KEYCODE = 27;
 
 // массив описаний
 var DESCRIPTIONS = [
@@ -41,7 +42,6 @@ var getLikes = function () {
   return getRandomNumber(MIN_LIKES, MAX_LIKES);
 };
 
-
 // получаем один комментарий
 var generateOneComment = function () {
   var comments = [];
@@ -62,7 +62,7 @@ var getComments = function () {
   return comments;
 };
 
-// создаем фотографии
+// создаем объект фотографии
 var generatePhoto = function (count) {
   var photos = [];
   for (var i = 0; i < count; i++) {
@@ -97,8 +97,7 @@ var appendPictures = function () {
 
 appendPictures();
 
-// задание №4
-
+// комментарии для большого фото
 // ищем готовую разметку в html для рандомных комментариев
 var commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
 var renderComment = function (comments) {
@@ -123,13 +122,58 @@ var renderBigPicture = function (photo) {
   bigPicture.querySelector('.big-picture__img').querySelector('img').src = photo.url;
   bigPicture.querySelector('.likes-count').textContent = photo.likes;
   bigPicture.querySelector('.comments-count').textContent = photo.comments.length;
-  bigPicture.querySelector('.social__caption').textContent = photos[0].description;
-  renderComment(photos[0].comments);
+  bigPicture.querySelector('.social__caption').textContent = photo.description;
+  renderComment(photo.comments);
 };
 
-renderBigPicture(photos[getRandomNumber(0, PHOTO_QUANTITY)]);
-
-// задание №5
-
+// прячем блок счетчиков комментариев
 document.querySelector('.social__comment-count').classList.add('visually-hidden');
+
+// прячем загрузку новых комментариев
 document.querySelector('.comments-loader').classList.add('visually-hidden');
+
+// находим маленькое изображение на странице
+var smallPhoto = document.querySelectorAll('.picture');
+
+// создаем функцию которая слушает клики по маленьким фотографиям
+// и делает их большими с помощью функции создания больших фотографий renderBigPicture
+var addSmallPhotoHadler = function (miniPhoto, bigPhoto) {
+  miniPhoto.addEventListener('click', function () {
+    renderBigPicture(bigPhoto);
+  });
+};
+
+// запускаем цикл в котором вызовется функция (которая слушает клики по маленьким фотографиям)
+for (var i = 0; i < smallPhoto.length; i++) {
+  addSmallPhotoHadler(smallPhoto[i], photos[i]);
+}
+
+// закрываем большую фотографию крестиком
+var bigPicture = document.querySelector('.big-picture');
+var buttonClose = document.querySelector('.big-picture__cancel');
+buttonClose.addEventListener('click', function () {
+  bigPicture.classList.add('hidden');
+});
+
+// закрываем большую фотографию esc
+document.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    bigPicture.classList.add('hidden');
+    // закрываем форму редактирования изображения
+    imageEdit.classList.add('hidden');
+  }
+});
+
+// открываем форму редактирования изображения при действие change в форме загрузки новых фотографий
+var uploadFile = document.querySelector('#upload-file');
+var imageEdit = document.querySelector('.img-upload__overlay');
+uploadFile.addEventListener('change', function () {
+  imageEdit.classList.remove('hidden');
+  uploadFile.value = '';
+});
+
+// закрываем форму редактирования изображения крестиком
+var imageEditCloseBtn = document.querySelector('.img-upload__cancel');
+imageEditCloseBtn.addEventListener('click', function () {
+  imageEdit.classList.add('hidden');
+});
